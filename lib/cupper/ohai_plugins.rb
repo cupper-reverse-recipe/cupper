dbc485b0411cc6a8f19d903b56596761e25d675a
Paulo Tada <paulohtfs@gmail.com>
2016-08-02 01:20:00 -0300

    Fixing ohai_plugins list method

--- lib/cupper/ohai_plugins.rb
+++ lib/cupper/ohai_plugins.rb
@@ -6,7 +6,7 @@ module Cupper
     def list
       @plugins_path = File.expand_path '../../cupper/plugins/ohai', __FILE__
       @plugins = Dir.entries(@plugins_path).reject{ |entry| entry == '.' || entry == '..'  }
-      @plugins.each { |plugin| plugin.chomp(".rb") }
+      @plugins.each { |plugin| plugin.chomp!(".rb") }
       @plugins
     end
   end

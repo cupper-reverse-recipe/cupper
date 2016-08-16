ed5e173e3f9dca2507f674768500abf79ca0c998
Paulo Tada <paulohtfs@gmail.com>
2016-08-15 23:57:02 -0300

    First model of templates

--- lib/cupper.rb
+++ lib/cupper.rb
@@ -4,4 +4,11 @@ require 'cupper/cli'
 require 'cupper/ohai_plugins'
 require 'cupper/collect'
 require 'cupper/manager'
+require 'cupper/entity'
+
+module Cupper
+  CUPPER_ROOT       = File.expand_path(File.dirname(__FILE__))
+  OHAI_PLUGINS_PATH = CUPPER_ROOT + '/cupper/plugins/ohai'
+  TEMPLATE_PATH     = CUPPER_ROOT + '/cupper/templates'
+end
 
--- lib/cupper/entity.rb
+++ lib/cupper/entity.rb
@@ -1,34 +1,84 @@
 module Cupper
-  # Entity can be a directory or file
-  class Entity
+  # Entity represents a Entity
+  module Entity
+    require 'erb'
+
     FILE = 'file'
     DIR = 'dir'
 
-    # Contructor
-    def initialize(type)
-      @destination_path = ''
-      @file_or_dir = type
+    # As default the Entity is treated as a file
+    def initialize
+      @type = FILE
     end
 
     # Create the actual file or dir in the correct place
-    def create
+    def create(name, dest_path, type = nil)
+      config(name, dest_path, type)
+      content('_package.erb')
+      save
+    end
+
+    # Set the attributes of the Entity
+    def config(name, dest_path, type)
+      @name = name
+      @type = type if not type.nil?
+      @dest_path = dest_path
+      @full_path = "#{dest_path}/#{name}"
     end
 
     # Returns the content of the file
-    def content
-      return false if self.file?
+    def content(erb_file)
+      return false if self.dir?
+      @template = File.read(TEMPLATE_PATH + "/#{erb_file}")
     end
 
-    # Returns the template of the file
-    def template
+    def render
+      ERB.new(@template).result(binding)
     end
 
-    # There is a difference between file or directory
+    def save
+      File.open(@full_path,"a+") do |f|
+        f.write(render)
+      end
+    end
+
+    # Treats entity as a file or as a dir
     def file?
-      @file_or_dir == FILE
+      @type == FILE
     end
     def dir?
-      @file_or_dir == DIR
+      @type == DIR
+    end
+  end
+
+  class Attribute
+    attr_reader :attr
+  end
+
+  # Represents the recipe of the cookbook
+  class Recipe
+    include Cupper::Entity
+    def initialize
+      @packages = Array.new
+      setup
+    end
+
+    def setup
+      @packages.push(new_package("pgsql","1.2.3"))
+      @packages.push(new_package("mysql","3.5.3"))
+      @packages.push(new_package("vim","4.2.1"))
+      @packages
+    end
+
+    def new_package(name, version)
+      package = Attribute.new
+      class << package
+        attr_accessor :name
+        attr_accessor :version
+      end
+      package.name = name
+      package.version = version
+      package
     end
   end
 end

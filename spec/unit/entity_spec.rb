20895f38143d2445fe332dccf84462affe5a7f84
Paulo Tada <paulohtfs@gmail.com>
2016-08-23 00:35:54 -0300

    Refactoring test and adding skeleton from 2 more test suit

---
+++ spec/unit/entity_spec.rb
@@ -0,0 +1,11 @@
+describe Cupper::Entity do
+  it 'should create a valid Entity mixin'
+  it 'should has name and dest_path'
+  it 'should return a content of ERB file'
+  it 'should render the template'
+  it 'should be a file'
+  it 'should be a dir'
+  it 'should exist'
+  it 'should not exist'
+  it 'should save template'
+end
---
+++ spec/unit/ohai_plugins_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Cupper::OhaiPlugin do
+  it 'should return a list of plugins from ohai'
+end

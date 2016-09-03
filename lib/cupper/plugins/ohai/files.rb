2643fe20e6d791c6a4588f0b4c8ef726cb720df2
Paulo Tada <paulohtfs@gmail.com>
2016-09-03 18:41:41 -0300

    Adding "files" ohai plugin

---
+++ lib/cupper/plugins/ohai/files.rb
@@ -0,0 +1,31 @@
+Ohai.plugin(:Files) do
+  provides 'files'
+
+  def from_cmd(cmd)
+    so = shell_out(cmd)
+    so.stdout.lines
+  end
+
+  def extract_files
+    subdir = true
+    cmd = 'file /etc/**'
+    result = Array.new
+    while subdir do
+      result << from_cmd(cmd)
+      cmd += '/**'
+      result.flatten!
+      subdir = false if result.last.match('cannot open')
+    end
+    result
+  end
+
+  collect_data(:linux) do
+    files Mash.new
+    extract_files.each do |file|
+      path, type = file.split(' ', 2)
+      files[path] = {
+        'type' => type
+      }
+    end
+  end
+end
